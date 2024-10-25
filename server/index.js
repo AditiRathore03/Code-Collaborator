e1a5e86d103b570f09adb2e5ab7cf9ccb83a7970
AditiRathore03 <131246134+AditiRathore03@users.noreply.github.com>
2024-10-25 15:04:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", 
+    origin: "https://vercel.com/aditi-rathores-projects/code-collaborator-jwob/G1HFDhqY1sWzWiLF2jTHEyTzj8bx", 
     methods: ["GET", "POST"]
   }
 });
